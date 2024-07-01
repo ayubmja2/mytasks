a3bb957099b5a26034cc3b592b89133619db8ad3
Ayub <ayubmja@icloud.com>
2024-07-01 14:13:57 -0400

    I can post new task now -> that works

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -20,7 +20,7 @@ class TaskController extends Controller
      */
     public function create()
     {
-        //
+        return view('tasks.index');
     }
 
     /**
@@ -28,7 +28,15 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name
+        ]);
+
+        return redirect()->route('tasks');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ Route::get('/tasks', function () {
     return view('tasks');
 })->middleware(['auth', 'verified'])->name('tasks');
 
+
+
 Route::get('post', function() {
     return view('post.index');
 })->middleware(['auth', 'verified'])->name('post');
@@ -20,6 +22,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 });
 
 require __DIR__.'/auth.php';
